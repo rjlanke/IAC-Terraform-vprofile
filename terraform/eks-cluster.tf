f5a6dedda296478d8ccab160116522da39299cee
rjgituser <rajeshlanke1@gmail.com>
2026-01-22 17:22:14 +0530

    fixed indentation from chatgpt

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -17,19 +17,17 @@ module "eks" {
     one = {
       name           = "node-group-1"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
     }
 
     two = {
       name           = "node-group-2"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
 }
