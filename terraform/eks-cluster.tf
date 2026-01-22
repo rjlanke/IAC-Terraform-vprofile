b08aff39b2ec828ba784c3adceb4b80e7f562673
rjgituser <rajeshlanke1@gmail.com>
2026-01-22 17:27:00 +0530

    Fixed eks-cluster file by recreating it

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
-  cluster_name                   = local.cluster_name
-  cluster_version                = "1.27"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
@@ -31,3 +31,4 @@ module "eks" {
     }
   }
 }
+
